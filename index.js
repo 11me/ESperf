e70a99eb696ebf0d6b28aa159d95d8f38b55409f
11me <osmium760@gmail.com>
2021-03-24 21:32:15 +0300

    update output format

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const measure = (n, stream, callback, args) => {
     while (n) {
         const start = performance.now();
         args ? callback(...args) : callback();
-        stream.write(`${performance.now() - start},\n`);
+        stream.write(`${performance.now() - start}\n`);
         n--;
     }
 }
@@ -29,7 +29,7 @@ const out = 'output';
 // measure in the loop and save the results
 for (mod of loadedModules) {
     const writeStream = fs.createWriteStream(`${out}/${mod.name}.csv`);
-    writeStream.write(`${mod.description},\n`);
+    writeStream.write(`${mod.description}\n`);
     mod.args ? measure(iterations, writeStream, mod.call, mod.args)
         : measure(iterations, writeStream, mod.call, mod.args);
 }
